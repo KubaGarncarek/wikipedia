7e22f8e443d591d65245ec99e291d258e03df51c
KubaGarncarek <kubagarncarek47@gmail.com>
2022-06-19 21:52:28 +0200

    implementation visiting /wiki/TITLE and update index.html such that user can click on entry name to be taken directly to that entry page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,4 +7,18 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+def greet(request,name):
+    name = name.lower()
+    entries = util.list_entries()
+    for entry in range(len(entries)):
+        entries[entry] = entries[entry].lower()
+    if name in entries:
+        return render(request, "encyclopedia/greet.html", {
+            "entry" : util.get_entry(name),
+            "title" : name
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "message" : f"page {name} does not exist"
+        })
 
