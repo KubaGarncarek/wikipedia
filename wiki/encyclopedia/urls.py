599dcd8fdc0392f66a6efd2e0e97f6b873cb495e
KubaGarncarek <kubagarncarek47@gmail.com>
2022-06-20 14:23:00 +0200

    complete search

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:name>", views.greet, name="greet"),
+    path("search", views.search, name="search"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django import forms
 from . import util
+
+def lowercase_entries():
+    entries = util.list_entries()
+    for entry in range(len(entries)):
+        entries[entry] = entries[entry].lower()
+    return entries
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 def greet(request,name):
     name = name.lower()
-    entries = util.list_entries()
-    for entry in range(len(entries)):
-        entries[entry] = entries[entry].lower()
-    if name in entries:
+    if name in lowercase_entries():
         return render(request, "encyclopedia/greet.html", {
             "entry" : util.get_entry(name),
             "title" : name
@@ -22,3 +25,21 @@ def greet(request,name):
             "message" : f"page {name} does not exist"
         })
 
+def search(request):
+    if request.method == "POST":
+        searched_entry = request.POST.get('q').lower()
+        
+        if searched_entry in lowercase_entries():
+            return render(request,"encyclopedia/greet.html",{
+                "entry" : util.get_entry(searched_entry),
+                "title" : searched_entry
+            })
+        entries = []
+        for entry in lowercase_entries():
+            if searched_entry in entry:
+                entries.append(entry)
+        if entries:
+            return render(request, "encyclopedia/index.html", {
+                "entries": entries,
+            })
+        return render(request, "encyclopedia/index.html")
